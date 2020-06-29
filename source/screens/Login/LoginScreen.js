a54a5074b59be22cc35583abadeb349fbdc886e4
austinckohler <austinckohler@gmail.com>
2020-06-28 22:56:24 -0600

    still not working going to bed

--- source/screens/Login/LoginScreen.js
+++ source/screens/Login/LoginScreen.js
@@ -1,51 +1,50 @@
-// import React, {useState, useEffect} from 'react';
-// import {View, Text} from 'react-native';
-// import auth from '@react-native-firebase/auth';
-// import {db} from '../../config';
+import React, {useState, useEffect} from 'react';
+import {View, Text} from 'react-native';
+import auth from '@react-native-firebase/auth';
 
-// function Login() {
-//   // Set an initializing state whilst Firebase connects
-//   const [initializing, setInitializing] = useState(true);
-//   const [user, setUser] = useState();
+function Login() {
+  // Set an initializing state whilst Firebase connects
+  const [initializing, setInitializing] = useState(true);
+  const [user, setUser] = useState();
 
-//   // Handle user state changes
-//   function onAuthStateChanged(user) {
-//     setUser(user);
-//     if (initializing) setInitializing(false);
-//   }
+  // Handle user state changes
+  function onAuthStateChanged(user) {
+    setUser(user);
+    if (initializing) setInitializing(false);
+  }
 
-//   auth()
-//     .signInAnonymously()
-//     .then(() => {
-//       console.log('User signed in anonymously');
-//     })
-//     .catch(error => {
-//       if (error.code === 'auth/operation-not-allowed') {
-//         console.log('Enable anonymous in your firebase console.');
-//       }
+  auth()
+    .signInAnonymously()
+    .then(() => {
+      console.log('User signed in anonymously');
+    })
+    .catch(error => {
+      if (error.code === 'auth/operation-not-allowed') {
+        console.log('Enable anonymous in your firebase console.');
+      }
 
-//       console.error(error);
-//     });
+      console.error(error);
+    });
 
-//   useEffect(() => {
-//     const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
-//     return subscriber; // unsubscribe on unmount
-//   }, []);
+  useEffect(() => {
+    const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+    return subscriber; // unsubscribe on unmount
+  }, []);
 
-//   if (initializing) return null;
+  if (initializing) return null;
 
-//   if (!user) {
-//     return (
-//       <View>
-//         <Text>Login</Text>
-//       </View>
-//     );
-//   }
+  if (!user) {
+    return (
+      <View>
+        <Text>Login</Text>
+      </View>
+    );
+  }
 
-//   return (
-//     <View>
-//       <Text>Welcome {user.email}</Text>
-//     </View>
-//   );
-// }
-// export default Login;
+  return (
+    <View>
+      <Text>Welcome {user.email}</Text>
+    </View>
+  );
+}
+export default Login;
